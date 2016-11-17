31ca7eacda28308f32565091f3444208c46e9ff6
Jose Quinteiro <jq@quinteiro.org>
2016-11-16 21:26:55 -0800

    Fix mac compilation with clang

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -418,7 +418,7 @@ class HonorStanding
         HonorStanding* GetInfo() { return this; };
 
         // create the standing order
-        bool operator < (const HonorStanding& rhs)
+        bool operator < (const HonorStanding& rhs) const
         {
             return honorPoints > rhs.honorPoints;
         }
